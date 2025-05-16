dde25259b0548350bf0989185b0ec32a0681336a
1973034392 <1973034392@qq.com>
2025-05-16 17:18:36 +0800

    初始项目

---
+++ api-gateway-core/src/main/java/top/codelong/apigatewaycore/ApiGatewayCoreApplication.java
@@ -0,0 +1,13 @@
+package top.codelong.apigatewaycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayCoreApplication.class, args);
+    }
+
+}
