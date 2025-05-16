c8163d602c760cbdfc6186abbec4aebe6f64f624
1973034392 <1973034392@qq.com>
2025-05-16 17:01:01 +0800

    初始项目

---
+++ api-gateway-center/src/main/java/top/codelong/apigatewaycenter/ApiGatewayCenterApplication.java
@@ -0,0 +1,11 @@
+package top.codelong.apigatewaycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayCenterApplication.class, args);
+    }
+}
